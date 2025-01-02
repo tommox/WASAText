ea96c6b44aba0a8b3e23b8d85279708617390b43
tommox <tommi.salvi29@gmail.com>
2025-01-02 17:18:24 +0100

    correzioni api

--- service/api/utils.go
+++ service/api/utils.go
@@ -7,6 +7,6 @@ import (
 // verifica se l'id di un utente ha la lunghezza giusta
 func validIdentifier(identifier string) bool {
 	var nospace_id = strings.ReplaceAll(identifier, " ", "")
-	// 5-25 come def in API
+	// 3-16 come def in API
 	return len(identifier) >= 3 && len(identifier) <= 16 && nospace_id != "" && !strings.ContainsAny(nospace_id, "?")
 }
