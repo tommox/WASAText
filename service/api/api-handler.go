55e3f645fc58937e570fce22a3c3466a37a32bbb
tommox <tommi.salvi29@gmail.com>
2025-01-19 20:11:15 +0100

    .

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -6,7 +6,8 @@ import (
 
 // Handler returns an instance of httprouter.Router that handle APIs registered here
 func (rt *_router) Handler() http.Handler {
-	// Rotte "demo"
+
+	// Register routes
 	rt.router.GET("/", rt.getHelloWorld)
 	rt.router.GET("/context", rt.wrap(rt.getContextReply))
 	rt.router.GET("/liveness", rt.liveness)
