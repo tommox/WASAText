df0cb7c6066459a25e4383682e2caeb25b6f96fa
tommox <tommi.salvi29@gmail.com>
2025-01-28 16:29:23 +0100

    inizio frontend

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -5,9 +5,6 @@ const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
 	routes: [
 		{path: '/', component: HomeView},
-		{path: '/link1', component: HomeView},
-		{path: '/link2', component: HomeView},
-		{path: '/some/:id/link', component: HomeView},
 	]
 })
 
