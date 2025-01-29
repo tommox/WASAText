5976fcb850125fc4d0c038e6d11e7ec807c32468
tommox <tommi.salvi29@gmail.com>
2025-01-29 19:28:10 +0100

    immagine login

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -5,6 +5,7 @@ import LoginView from '../views/LoginView.vue'
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
 	routes: [
+		{path: '/', redirect: '/login'},
 		{path: '/home', component: HomeView},
 		{path: '/login', component: LoginView},
 	]
