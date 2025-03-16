738c1b082975c35e7a9d7b2153fed964053abe38
tommox <tommi.salvi29@gmail.com>
2025-03-16 18:00:16 +0100

    .

--- service/database/get-convid-msgid.go
+++ service/database/get-convid-msgid.go
@@ -11,3 +11,17 @@ func (db *appdbimpl) GetConversationIdByMessageId(messageId int) (int, error) {
 	}
 	return conversationId, nil
 }
+
+func (db *appdbimpl) GetGroupIdByGroupMessageId(messageId int) (int, error) {
+	query := `
+		SELECT Group_id 
+		FROM GroupMessages
+		WHERE GroupMessage_id = ?
+	`
+	var groupId int
+	err := db.c.QueryRow(query, messageId).Scan(&groupId)
+	if err != nil {
+		return 0, fmt.Errorf("GetGroupIdByGroupMessageId: %w", err)
+	}
+	return groupId, nil
+}
